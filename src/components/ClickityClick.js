5406ac329adc32b2d8b841d41c258e8fb6068b47
LainaJ <lainakarosic@gmail.com>
2019-12-12 22:06:36 -0500

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,33 @@
 // Code ClickityClick Component Here
+import React from 'react';
+ 
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+ 
+    // Define the initial state:
+    this.state = {
+      hasBeenClicked: false
+    };
+  }
+ 
+  handleClick = () => {
+    // Update our state here...
+    this.setState({
+        hasBeenClicked: true
+     }, () => console.log(this.state.hasBeenClicked))
+  };
+  //this added allBack function above makes sure that state is updated
+  //the syntax of line 18?? why, how would I write it as a separate callback function 
+ 
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
+ 
+export default ClickityClick;
